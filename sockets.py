d8678564c3df47dc158775ee24452703208d4994
Peter Elliott <pelliott@ualberta.ca>
2020-03-20 20:00:17 -0600

    did assigment

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -22,7 +22,7 @@ import time
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static/')
 sockets = Sockets(app)
 app.debug = True
 
@@ -31,7 +31,7 @@ class World:
         self.clear()
         # we've got listeners now!
         self.listeners = list()
-        
+
     def add_set_listener(self, listener):
         self.listeners.append( listener )
 
@@ -55,21 +55,26 @@ class World:
 
     def get(self, entity):
         return self.space.get(entity,dict())
-    
+
     def world(self):
         return self.space
 
-myWorld = World()        
+myWorld = World()
 
+
+socks = set()
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    for ws in socks:
+        if not ws.closed:
+            ws.send(json.dumps({entity: data}))
 
 myWorld.add_set_listener( set_listener )
-        
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
@@ -80,8 +85,20 @@ def read_ws(ws,client):
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    socks.add(ws)
+    while not ws.closed:
+        m = ws.receive()
+        if m is None:
+            break
+        message = json.loads(m)
+        if "request" in message and message["request"] == "all":
+            for entity, data in myWorld.space.items():
+                ws.send(json.dumps({entity: data}))
+        else:
+            for key, val in message.items():
+                myWorld.set(key, val)
+
+    socks.remove(ws)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +116,25 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    myWorld.set(entity, flask_post_json())
+    return jsonify(myWorld.get(entity))
 
-@app.route("/world", methods=['POST','GET'])    
+@app.route("/world", methods=['POST','GET'])
 def world():
     '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world())
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.world())
 
 
 
